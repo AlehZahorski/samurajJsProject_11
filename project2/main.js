aa08ec4bd23ee5066993f98e9f93b7395133a786
AlehZahorski <git3371m@gmail.com>
2020-05-20 20:16:40 +0200

    recode

--- project2/main.js
+++ project2/main.js
@@ -1,4 +1,4 @@
-const endTime = new Date('2021-01-25 22:34:00').getTime();
+const endTime = new Date('2020-12-31 23:59:59').getTime();
 const spanDays = document.querySelector('span.d');
 const spanHours = document.querySelector('span.h');
 const spanMinutes = document.querySelector('span.m');
@@ -6,13 +6,16 @@ const spanSecunds = document.querySelector('span.s');
 
 function a(){
     const nowTime = new Date().getTime();
-    const days = (endTime/(1000*60*60*24)) - (nowTime/(1000*60*60*24));
-    const hours = ((endTime/(1000*60*60)) - (nowTime/(1000*60*60)))%24
-    const minutes = ((endTime/1000*60) - (nowTime/1000*60))%60;
-    const secunds = ((endTime/1000) - (nowTime/1000))%60;
+    let days = (endTime/(1000*60*60*24)) - (nowTime/(1000*60*60*24));
+    let hours = Math.floor(((endTime/(1000*60*60)) - (nowTime/(1000*60*60)))%24);
+    hours = hours < 10 ? `0${hours}` : hours;
+    let minutes = Math.floor(((endTime/1000*60) - (nowTime/1000*60))%60);
+    minutes = minutes < 10 ? `0${minutes}` : minutes;
+    let secunds = Math.floor(((endTime/1000) - (nowTime/1000))%60);
+    secunds = secunds < 10 ? `0${secunds}` : secunds;
     spanDays.textContent = (days.toFixed());
-    spanHours.textContent = (hours.toFixed());
-    spanMinutes.textContent = (minutes.toFixed());
-    spanSecunds.textContent = (secunds.toFixed());
+    spanHours.textContent = (hours);
+    spanMinutes.textContent = (minutes);
+    spanSecunds.textContent = (secunds);
 }
 setInterval(a,1000)
