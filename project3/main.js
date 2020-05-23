125a120c0c03b1fdb1c55bae3972e635875fe3b8
AlehZahorski <git3371m@gmail.com>
2020-05-23 11:52:19 +0200

    done, its working

--- project3/main.js
+++ project3/main.js
@@ -4,26 +4,45 @@ const div = document.querySelector('.time div');
 
 let msec = 0;
 let sec = 0;
+let min = 0;
+let flag = false;
+let startTimer;
 
 const timer = () =>{
     msec++;
-    div.textContent = msec < 10 ? `${sec}:0${msec}` : `${sec}:${msec}`
+    div.textContent = msec < 10 ? `0${min}:0${sec}:0${msec}` : `${min}:${sec}:${msec}`
     if(msec%100 === 0){
         msec = 0;
         sec++;
+        if(sec%60 ===0){
+            sec = 0;
+            min++;
+        }
     }
 }
 
 const start = () => {
-    div.textContent = '';
-    btnStart.textContent = 'Paused';
-    const startTimer = setInterval(timer, 10);
-    btnReset.addEventListener('click', ()=>{
+    
+    if(!flag){
+        startTimer = setInterval(timer, 10);
+        flag = !flag
+        div.textContent = '';
+        btnStart.textContent = 'Paused';
+    }else{
+        flag = !flag
+        btnStart.textContent = 'Start';
         clearInterval(startTimer);
+    }
+
+    btnReset.addEventListener('click', ()=>{
+        flag = false;
+        clearInterval(startTimer);
+        btnStart.textContent = 'Start';
         sec = 0;
         msec = 0;
         div.textContent = '---';
     })
+    
 }
 
 btnStart.addEventListener('click', start)
